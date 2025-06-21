f088b214f34e7e59d215029cf8dd68d398ccd469
BotchaVarun <varunbotcha@gmail.com>
2025-06-21 15:38:12 +0530

    requirements updated

--- settings.py
+++
@@ -1,6 +0,0 @@
-def get_version():
-    try:
-        # your existing version reading code
-        return version_dict['__version__']
-    except KeyError:
-        return "0.1.0"  # fallback version
